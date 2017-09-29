02e43b6c6f65ae8e55e3d6a27e112092d030a646
andre-Hazim <andre.hazim@ocsbstudent.ca>
2017-09-29 13:59:47 -0400

    Create School_names_teams.swift

---
+++ School_names_teams.swift
@@ -0,0 +1,19 @@
+// created by andre hazim
+// created on sept 2017
+// crested for ics3u
+
+var motherTersea = "Mother Teresa"
+var stJoes = "St. Joes"
+var stMark = "St. Mark"
+show("What school name and team do you want?")
+show ("The choices are Mother Teresa, St. Joes, and St. Mark")
+let schoolName = ask("school")
+
+if schoolName == motherTersea {
+    show("Mother Teresa Titans")
+    
+} else if schoolName == stJoes {
+    show("St. Joe's Jaguars")
+} else if schoolName == stMark{
+show("St. Mark's Lions")
+}
